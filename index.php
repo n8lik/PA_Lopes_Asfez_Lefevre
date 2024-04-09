c5121884ce7e0c296a55613ae3f5bcb24f27ac87
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2024-04-09 17:09:18 +0200

    refractor : début code index

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
 <?php
 	include 'includes/header.php';
-	?>
-	<div class="container">
-		<div class="header">
-			<h1>PA_Lopes_Asfez_Lefevre</h1>
-		</div>
-		<div class="menu">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="contact.php">Contact</a></li>
-			</ul>
-		</div>
-		<div class="content">
-			<h2>Home</h2>
-			<p>Welcome to our website!</p>
-		</div>
+?>
+<link rel="stylesheet" type="text/css" href="css\index.css">
+<div class="index-container">
+	<div class="index-container-name">
+		<p>Logement</p>
 	</div>
-
-	<!--aller a prestatire.php pour créer un prestatire-->
-	<a href="prestatire.php">Créer un prestatire</a>
-	
+	<div class="index-container-name">
+		<p>Activités</p>
+	</div>
+</div>
 
 <?php
 	include 'includes/footer.php';
