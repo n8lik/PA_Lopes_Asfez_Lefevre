28c977e6607d534e2c393dc0faa3bc5ced90bb42
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2024-04-10 17:05:19 +0200

    refractor : admin part

---
+++ admin/admin_header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PCS Admin </title>
+	<link rel="stylesheet" type="text/css" href="..\css\base.css">
+    <link rel="stylesheet" type="text/css" href="admin.css">
+</head>
+	<header class="header">
+		<img src="../assets/logos/darkLogo.png" alt="logo" width="10%">
+        <!--Afficher espace ADMIN-->
+		<div class="burger-menu">
+			<div class="line"></div>
+			<div class="line"></div>
+			<div class="line"></div>
+		</div>
+
+
+	</header>
+
+<body class="body">
