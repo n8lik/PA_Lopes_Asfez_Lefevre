5281b8e4585464d4c61ad1931a4edcfc9e22e922
Lopes Angélique <123382274+meeaav@users.noreply.github.com>
2024-04-17 15:35:49 +0200

    style : modif headers

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -11,48 +11,68 @@
 </head>
 <body class="body bg-light">
     <header class="header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">
                     <img src="../assets/logos/darkLogo.png" alt="Logo" height="70">
                 </a>
+				<!-- faire en sorte que le burger menu soit tout a droite -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">En attente de validation</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">En attente de validation</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="#">Prestataires</a></li>
+                                <li><a class="dropdown-item" href="#">Prestations</a></li>
+                                <li><a class="dropdown-item" href="#">Bailleurs</a></li>
+                                <li><a class="dropdown-item" href="#">Logements</a></li>
+                                <li><a class="dropdown-item" href="#">Evolution tarifaire</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Voir tout</a></li>
+                             </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Annonces</a>
+                        
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Gestion annonces</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="#">Logements</a></li>
+                                <li><a class="dropdown-item" href="#">Prestations</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Voir tout</a></li>
+                             </ul>
                         </li>
-						
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Gestion utilisateurs</a>
+                        
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Gestion utilisateurs</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="#">Prestataires</a></li>
+                                <li><a class="dropdown-item" href="#">Bailleurs</a></li>
+                                <li><a class="dropdown-item" href="#">Voyageurs</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Voir tout</a></li>
+                             </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tickets</a>
-                        </li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Tickets</a>
+						</li>
                     </ul>
                 </div>
             </div>
         </nav>
-        <nav class="navbar navbar-expand bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
             <div class="container-fluid justify-content-end">
-				// If the user is connected, display the logout button
 				<?php if(isset($_SESSION['role'])) { ?>
-					
-					<a class="nav-link" href="#">Profil</a>
 					<span class="navbar-text">
-                    	<a href="login.php" class="btn btn-primary">Deconnexion</a>
+                    	<a href="login.php" class="btn btn-primary"><img src="..\assets\img\logout.png" </a>
                 	</span>
 				<?php } ?>
-
-
-                <span class="navbar-text">
-                    <a href="login.php" class="btn btn-primary">Deconnexion</a>
-                </span>
             </div>
         </nav>
     </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
