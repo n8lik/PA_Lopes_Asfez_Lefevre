af07c1167ada6513ea0b5d8f8d1c13a0745b5ed1
mike <160223887+Coatel5@users.noreply.github.com>
2024-04-29 10:55:14 +0200

    feat: début de prestations et changement login

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -18,11 +18,18 @@ is_validated
 
 */
 //récupération des DB_HOST, DB_DATABASE, DB_USER, DB_PWD, DB_PORT
-include 'conf.inc.php';
+
+define("DB_PREFIX","pcsall_");
+define("DB_DATABASE","pcsall_bdd");
+define("DB_USER","root");
+define("DB_PWD","");
+define("DB_PORT","3306");
+define("DB_HOST","localhost");
+
+
 
 // fonction connexion base de donnée 
-function connectDB()
-{
+function connectDB(){
 
     try {
         $connection = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";port=" . DB_PORT, DB_USER, DB_PWD);
@@ -75,3 +82,23 @@ function cleanFirstName($name)
     return $name;
 }
 
+// fonction pour calculer les prix 
+
+function price_calcul($type){
+    
+}
+
+
+
+function ajouterPrestation($performance_type, $title, $description, $date_debut, $date_fin, $zip_code, $price, $city, $address, $country) {
+    $conn = connectDB();
+    $sql = "INSERT INTO " . DB_PREFIX . "prestation (performance_type, title, description, date_deb, date_fin, zip_code, price, city, address, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    try {
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$performance_type, $title, $description, $date_debut, $date_fin, $zip_code, $price, $city, $address, $country]);
+        return true;
+    } catch (PDOException $e) {
+        echo "Erreur lors de l'ajout de la prestation : " . $e->getMessage();
+        return false;
+    }
+}
